fca266ac2b4c1947326662c2e0526021b8c98c5b
Xpamoff <2005cramar.05@gmail.com>
2022-04-28 18:17:35 +0400

    Added country database

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, redirect
+from flask_sqlalchemy import SQLAlchemy
 import compare
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///geodraw.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-actual_data = []
-
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(240), nullable=False)
+    path = db.Column(db.String(240), nullable=False)
+    region = db.Column(db.String(240), nullable=False)
 
 @app.route('/')
 def index():
@@ -13,9 +20,12 @@ def index():
 
 @app.route('/compare', methods=['GET', 'POST'])
 def result():
-    url = request.form.get('url')
-    hash1, hash2, compared = compare.compare(url)
-    return render_template('compare.html', hash1=hash1, hash2=hash2, compared=compared)
+    if request.method == "POST":
+        url = request.form.get('url')
+        hash1, hash2, compared = compare.compare(url)
+        return render_template('compare.html', hash1=hash1, hash2=hash2, compared=compared)
+    else:
+        return redirect('/')
 
 
 if __name__ == '__main__':
